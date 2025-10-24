8aec29244ef2c274f8345c63722caf151ba8b281
szymonflis <szymon.flis@gmail.com>
2025-10-24 11:41:50 +0100

    Dynamic Array initial push

---
+++ src/dynamicArrays/DynamicArray.java
@@ -0,0 +1,55 @@
+package dynamicArrays;
+
+public class DynamicArray {
+
+    private int[] array;
+    private int capacity;
+    private int length;
+
+    public DynamicArray(int capacity) {
+        this.capacity = capacity;
+        this.array = new int[this.capacity];
+        this.length = 0;
+    }
+
+    public int get(int i) {
+        return array[i];
+    }
+
+    public void set(int i, int n) {
+        array[i] = n;
+    }
+
+    public void pushback(int n) {
+        if (length == capacity){
+            resize();
+        }
+        array[length] = n;
+        length ++;
+
+    }
+
+    public int popback() {
+        if (length > 0) {
+            length--;
+        }
+        return array[length];
+    }
+
+    private void resize() {
+        capacity = capacity * 2;
+        int[] resizedArray = new int[capacity];
+        for (int i = 0; i < length; i++) {
+            resizedArray[i] = array[i];
+        }
+        array = resizedArray;
+    }
+
+    public int getSize() {
+        return length;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
